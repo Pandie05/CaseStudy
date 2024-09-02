bcc0093835396a61f49c82cf757badf2cea826fc
Pandie05 <150094372+Pandie05@users.noreply.github.com>
2024-09-01 20:31:21 -0400

    yes

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("AHAHAHAHHA")
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+jail = pd.read_csv("CaseStudy/csv/cty_jail_allSubgroups.csv")
+grad = pd.read_csv("CaseStudy/csv/cty_hs_allSubgroups.csv")
+
+print(jail.head())
+print(grad.head())
