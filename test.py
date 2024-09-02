7f52b8843c272e6258fb4ddd49da45492239f36a
Pandie05 <hreyes@email.neit.edu>
2024-09-01 20:50:09 -0400

    getting started

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
-import matplotlib.pyplot as plt
-
-jail = pd.read_csv("CaseStudy/csv/cty_jail_allSubgroups.csv")
-grad = pd.read_csv("CaseStudy/csv/cty_hs_allSubgroups.csv")
+jail = pd.read_csv('csv\jail.csv')
+grad = pd.read_csv('csv\grad.csv')
 
 print(jail.head())
 print(grad.head())
